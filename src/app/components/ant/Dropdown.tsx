43250cd926d407daa44dbe33dcaf6aa09b9c1f23
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2024-11-17 21:33:55 +0100

    fix dropdown height

--- src/app/components/ant/Dropdown.tsx
+++ src/app/components/ant/Dropdown.tsx
@@ -2,19 +2,24 @@ import React from "react";
 import { DownOutlined } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Dropdown, Space, Typography } from "antd";
-import module_specs from "../../data/module_specs.json";
 import { ModuleSpec } from "@/app/data/typings";
 
-const SelectableDropdown = ({ moduleSpecs, setModuleSpecs }: { moduleSpecs: ModuleSpec[]; setModuleSpecs: any }) => {
+const SelectableDropdown = ({
+  moduleSpecs,
+  setModuleSpecs,
+}: {
+  moduleSpecs: ModuleSpec[];
+  setModuleSpecs: any;
+}) => {
   const items: MenuProps["items"] = moduleSpecs.map((moduleSpec) => ({
     key: moduleSpec.id.toString(),
     label: moduleSpec.name,
   }));
 
   const handleSelect: MenuProps["onSelect"] = (e) => {
-    console.log(e);
-    console.log(moduleSpecs);
-    const selectedModuleSpec = moduleSpecs.find((moduleSpec) => Number(moduleSpec.id) === Number(e.key) + 1);
+    const selectedModuleSpec = moduleSpecs.find(
+      (moduleSpec) => Number(moduleSpec.id) === Number(e.key)
+    );
     console.log(selectedModuleSpec);
   };
 
@@ -24,8 +29,8 @@ const SelectableDropdown = ({ moduleSpecs, setModuleSpecs }: { moduleSpecs: Modu
         items,
         selectable: true,
         onSelect: (e) => handleSelect(e),
+        style: { maxHeight: 400, overflowY: "auto" },
       }}
-      overlayStyle={{ height: 300 }}
     >
       <Typography.Link>
         <Space>Add module spec</Space>
